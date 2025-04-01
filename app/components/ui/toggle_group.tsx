27ea30e07ba267ea4bc5ab45df02a7f0de76443a
Hubert Lin <linhub15@users.noreply.github.com>
2025-04-01 14:35:43 -0600

    style: toggle option cursor-pointer

--- app/components/ui/toggle_group.tsx
+++ app/components/ui/toggle_group.tsx
@@ -13,13 +13,13 @@ import { Fragment, type PropsWithChildren } from "react";
 const radioStyles = {
   label: cn([
     // Base
-    "w-full inline-flex items-baseline justify-center gap-x-2 text-base/6 font-semibold select-none",
+    "w-full inline-flex items-baseline justify-center gap-x-2 text-base/6 font-semibold select-none cursor-pointer",
     // Sizing
     "px-[calc(--spacing(3.5)-1px)] py-[calc(--spacing(2.5)-1px)] sm:px-[calc(--spacing(3)-1px)] sm:py-[calc(--spacing(1.5)-1px)] sm:text-sm/6",
     // Focus
     "focus:outline-hidden data-focus:outline data-focus:outline-2 data-focus:outline-offset-2 data-focus:outline-blue-500",
     // Disabled
-    "data-disabled:opacity-50",
+    "data-disabled:opacity-50 data-disabled:cursor-default",
     // Icon
     "*:data-[slot=icon]:-mx-0.5 *:data-[slot=icon]:my-0.5 *:data-[slot=icon]:size-5 *:data-[slot=icon]:shrink-0 *:data-[slot=icon]:self-center *:data-[slot=icon]:text-(--btn-icon) sm:*:data-[slot=icon]:my-1 sm:*:data-[slot=icon]:size-4 forced-colors:[--btn-icon:ButtonText] forced-colors:data-hover:[--btn-icon:ButtonText]",
     // Active
