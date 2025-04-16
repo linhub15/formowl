36cc66f1b4e878fa8e8e15b3cd778ab9f4177aaf
Hubert Lin <linhub15@users.noreply.github.com>
2025-04-16 15:08:50 -0600

    feat: add empty state for forms page

--- app/routes/dashboard/forms/index.tsx
+++ app/routes/dashboard/forms/index.tsx
@@ -1,8 +1,11 @@
 import { SectionHeader } from "@/components/layout/section_header";
+import { Button } from "@/components/ui/button";
 import { Subheading } from "@/components/ui/heading";
+import { P } from "@/components/ui/text";
 import { useListForms } from "@/features/form_management/hooks/use_list_forms";
 import { cn } from "@/lib/utils/cn";
 import { DocumentTextIcon } from "@heroicons/react/20/solid";
+import { DocumentPlusIcon } from "@heroicons/react/24/outline";
 import { createFileRoute, Link } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/dashboard/forms/")({
@@ -10,13 +13,37 @@ export const Route = createFileRoute("/dashboard/forms/")({
 });
 
 function RouteComponent() {
-  const forms = useListForms();
+  const { data: forms, isLoading } = useListForms();
 
   return (
     <div className="space-y-8">
       <SectionHeader heading="Forms" />
+
+      {isLoading && (
+        <div className="flex items-center gap-3 py-4.5">
+          <DocumentTextIcon className="size-6 animate-pulse" />
+          <div className="space-y-3">
+            <div className="animate-pulse h-2.5 rounded-full bg-zinc-600 w-20" />
+            <div className="animate-pulse h-2.5 rounded-full bg-zinc-600 w-40" />
+          </div>
+        </div>
+      )}
+
+      {!isLoading && forms?.length === 0 && (
+        <div className="max-w-sm mx-auto text-center space-y-6 py-12">
+          <DocumentPlusIcon className="mx-auto size-16" />
+          <div className="space-y-1">
+            <Subheading className="sm:text-2xl/6">No forms yet</Subheading>
+            <P>Get started by creating a new form.</P>
+          </div>
+          <Button to="/dashboard/forms/create" variant="outline">
+            + New form
+          </Button>
+        </div>
+      )}
+
       <div className="space-y-3">
-        {forms.data?.map((form) => (
+        {forms?.map((form) => (
           <Link
             className={cn(
               "block p-4 -ml-4 rounded-lg",
